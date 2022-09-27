be8f3f0b0c2b37f09a476247ffae140ba148315b
dom111 <dom111@users.noreply.github.com>
2022-09-27 21:04:27 +0100

    Update dependencies and add generic property for `PlayerAction`.

--- AdjustTradeRates.d.ts
+++ AdjustTradeRates.d.ts
@@ -1,3 +1,4 @@
 import PlayerAction from '@civ-clone/core-player/PlayerAction';
-export declare class AdjustTradeRates extends PlayerAction {}
+import PlayerTradeRates from '@civ-clone/core-trade-rate/PlayerTradeRates';
+export declare class AdjustTradeRates extends PlayerAction<PlayerTradeRates> {}
 export default AdjustTradeRates;
--- AdjustTradeRates.ts
+++ AdjustTradeRates.ts
@@ -1,5 +1,6 @@
 import PlayerAction from '@civ-clone/core-player/PlayerAction';
+import PlayerTradeRates from '@civ-clone/core-trade-rate/PlayerTradeRates';
 
-export class AdjustTradeRates extends PlayerAction {}
+export class AdjustTradeRates extends PlayerAction<PlayerTradeRates> {}
 
 export default AdjustTradeRates;
